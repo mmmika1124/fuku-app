da29b8e26fe196a3d55d6106d27d7a46897eaa3f
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-04-03 12:36:35 +0000

    [Update]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,9 +6,17 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @customer = current_customer
+    @cart_items = @customer.cart_items
+    @order = Order.new(order_params)
+    @total = @cart_items.inject(0) { |total, cart_item| total + cart_item.subtotal }
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer_id
+    @order.save
+    redirect_to orders_thanks_path
   end
 
   def index
