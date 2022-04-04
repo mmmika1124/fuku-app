326be74730d5dd57cfdd2ef6681b8c63f6d5d1a3
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-04-04 06:34:07 +0000

    [Add]注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -32,9 +32,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module FukuApp
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
+    config.time_zone = 'Tokyo'
+
   end
 end
