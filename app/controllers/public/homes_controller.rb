aaf9583005708ebc979ad7375843b6478a7cdc31
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-04-08 08:41:52 +0000

    [Add]管理者トップページ

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     @items = Item.all.order(created_at: :desc)
+    @q = Genre.ransack(params[:q])
+    @results = @q.result
   end
 
   def about
